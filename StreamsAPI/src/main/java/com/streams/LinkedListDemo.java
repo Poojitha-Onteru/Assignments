f01d84dc31645f773d7a4fa8d6059ff45d75995b
Poojitha-Onteru <poojithaonteru@gmail.com>
2025-11-27 17:14:39 +0530

    nov27 1

---
+++ StreamsAPI/src/main/java/com/streams/LinkedListDemo.java
@@ -0,0 +1,16 @@
+package com.streams;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		//MyStack.pop();
+		MyStack.push();
+		MyStack.push();
+		MyStack.push();
+		MyStack.display();
+		MyStack.pop();
+		MyStack.pop();
+		MyStack.display();
+	}
+
+}
